97e5f41d7c4d73b9514d578c6045b914d97b4688
Faten Boughzala <faten05boughzala@gmail.com>
2025-12-09 18:58:08 +0100

    Update ALLOWED_HOSTS for additional domains

--- cv_project/settings.py
+++ cv_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-qxlyi)j-6o3_2k-v0buqy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'cv-rec3.onrender.com').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'cv-rec3.onrender.com,www.faten-boughzala-cv.com,faten-boughzala-cv.com,localhost,127.0.0.1').split(',')
 
 
 # Application definition
